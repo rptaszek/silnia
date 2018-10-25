ea7cab61830c7b0244c790dc2bbf7037d051e54d
rptaszek <rptaszek.gmail.com>
2018-10-25 14:26:17 +0200

    liczenie silni poprawione

--- main.c
+++ main.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 
-int licz_silnie(int podstawa) {
+long licz_silnie(int podstawa) {
     if (podstawa == 0) {
         return 1;
     } else {
-        int licznik;
-        int silnia;
+        long licznik;
+        long silnia = 1;
 
     for (licznik = 1 ; licznik <= podstawa ; ++licznik) {
         silnia *=licznik;
     }
     return silnia;
-}
+}}
 
 int main()
 {
-    int wynik;
-    int podstawa
+    long wynik;
+    long podstawa;
     printf("podaj podstawę silni 0 do 15: \n");
-    scanf("%i", &podstawa)
+    scanf("%ld", &podstawa);
 
     if (podstawa > 0 && podstawa <= 15){
         wynik = licz_silnie(podstawa);
-        printf("silnia : %i", wynik);
+        printf("silnia : %ld", wynik);
     }
     else{
         printf("podałeś liczbę z poza zakresu: \n");
